559ab8dd2ca5df035d94f4b0922facb2ed0bbb63
BaOmar278 <asma1412.sw@gmail.com>
2025-11-05 12:10:35 +0400

    add FKs

--- OmanClinc/Models/Appointment.cs
+++ OmanClinc/Models/Appointment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,16 @@ namespace OmanClinc.Models
 {
     public class Appointment
     {
+        [Key]
         public int AppointmentId { get; set; }
         public DateTime AppointmentDate { get; set; }
+
+        [ForeignKey("Patient")]
+        public string PatientId { get; set; }
         public Patient Patient { get; set; }
+
+        [ForeignKey("Doctor")]
+        public int DoctorId { get; set; }
         public Doctor Doctor { get; set; }
 
         public Appointment(int id, DateTime date, Patient patient, Doctor doctor)
--- OmanClinc/Models/Doctor.cs
+++ OmanClinc/Models/Doctor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,12 @@ namespace OmanClinc.Models
 {
     public class Doctor
     {
+        [Key]
         public int DoctorId { get; set; }
         public string FullName { get; set; }
         public string DoctorPhone { get; set; }
+        [ForeignKey("Specialty")]
+        public int SpecialtyId { get; set; }
         public Specialty Specialty { get; set; }
 
         public Doctor(int id, string fullName, string phone, Specialty specialty)
